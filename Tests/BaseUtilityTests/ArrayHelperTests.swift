f8c934dff14248a4f8783f432025e8319f572aa9
zakkhoyt <zakkhoyt@hatchbaby.com>
2024-05-18 01:38:31 -0600

    pulled main

--- Tests/BaseUtilityTests/ArrayHelperTests.swift
+++ Tests/BaseUtilityTests/ArrayHelperTests.swift
@@ -59,4 +59,87 @@ final class ArrayHelperTests: XCTestCase {
 //        dump(ddd)
         print("")
     }
+    
+    func testListDescriptions() {
+        struct Thing: CustomStringConvertible {
+            let name: String
+            let age: Int
+            
+            var description: String {
+                "\(name) \(age)"
+            }
+        }
+        let str: String = "String"
+        let point = CGPoint(x: 1, y: 1)
+        let thing = Thing(name: "me", age: 10)
+        let dict: [String: any CustomStringConvertible] = [
+            "a": "A",
+            "b": thing
+        ]
+        dump(dict)
+        let varD = dict.listDescription()
+        dump(varD)
+        ()
+        #warning("FIXME: zakkhoyt - use testing")
+    }
 }
+
+
+//extension Array where Element: CustomStringConvertible {
+//    public func listDescription(
+//        separator: String = ", ",
+//        endcaps: (String, String) = ("[", "]")
+//    ) -> String {
+//        let insert = separator == "\n" ? "\n" : ""
+//        return "\(endcaps.0)\(insert)\(map { $0.description }.joined(separator: separator))\(insert)\(endcaps.1)"
+//    }
+//    
+//    /// Converts any `[CustomStringConvertible]` to a comma delimited list (`String` ).
+//    public var listDescription: String {
+//        map { $0.description }.listDescription
+//    }
+//}
+//
+//extension Array where Element: CustomDebugStringConvertible {
+//    /// Converts any `[CustomDebugStringConvertible]` to a comma delimited list (`String` ).
+//    public var listDescription: String {
+//        map { $0.debugDescription }.listDescription
+//    }
+//}
+//
+//extension [String: any CustomStringConvertible] {
+//    public var dictDescription: String {
+//        //        compactMapValues { $0 }
+//        compactMapValues {
+//            //guard let value = $0 else { return "<nil>" }
+//            return String(describing: $0)
+//        }
+//        .map {
+//            [
+//                $0.key, $0.value
+//            ].joined(separator: ": ")
+//        }
+//        .listDescription
+////        .sorted()
+////        .joined(separator: ", ")
+//    }
+//    
+//    
+//    public func listDescription(
+//        separator: String = ", ",
+//        endcaps: (String, String) = ("[", "]")
+//    ) -> String {
+//        //        compactMapValues { $0 }
+//        compactMapValues {
+//            //guard let value = $0 else { return "<nil>" }
+//            return String(describing: $0)
+//        }
+//        .map {
+//            [
+//                $0.key, $0.value
+//            ].joined(separator: ": ")
+//        }.listDescription(separator: separator, endcaps: endcaps)
+//        
+//    }
+//}
+//
