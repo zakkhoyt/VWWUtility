c0564b2d3628e76a89d1e0be349b72761508e18f
Zakk Hoyt <zakkhoyt@hatchbaby.com>
2024-08-29 14:07:41 -0600

    bezier (#11)
    * converted bezier/polynomial and friends to public
    
    * Formatting

--- Sources/DrawingUI/Math/Line.swift
+++ Sources/DrawingUI/Math/Line.swift
@@ -8,20 +8,20 @@
 import CoreGraphics
 import Foundation
 
-struct Line: Identifiable {
-    var id: CGFloat {
+public struct Line: Identifiable {
+    public var id: CGFloat {
         point0.x + point0.y + point1.x + point1.y
     }
     
     /// An alternative to our `id` property which can certainly have duplicates.
-    var uuid: UUID {
+    public var uuid: UUID {
         UUID()
     }
     
-    let point0: CGPoint
-    let point1: CGPoint
+    public let point0: CGPoint
+    public let point1: CGPoint
 
-    init(
+    public init(
         point0: CGPoint,
         point1: CGPoint
     ) {
@@ -29,7 +29,7 @@ struct Line: Identifiable {
         self.point1 = point1
     }
 
-    init(
+    public init(
         x0: CGFloat,
         y0: CGFloat,
         x1: CGFloat,
@@ -41,18 +41,18 @@ struct Line: Identifiable {
 }
 
 extension Line {
-    var deltaX: CGFloat {
+    public var deltaX: CGFloat {
         point1.x - point0.x
     }
 
-    var deltaY: CGFloat {
+    public var deltaY: CGFloat {
         point1.y - point0.y
     }
 }
 
 extension Line {
     /// - Returns: Angle (in radians) of a line between two points and in terms of 0 ... 2*CGFloat.pi
-    var angle: CGFloat {
+    public var angle: CGFloat {
         CGFloat.project(
             angle: atan2(
                 point1.y - point0.y,
@@ -61,12 +61,12 @@ extension Line {
         )
     }
 
-    var reverseAngle: CGFloat {
+    public var reverseAngle: CGFloat {
         CGFloat.project(angle: angle - CGFloat.pi)
     }
 
     /// - Returns: Length of the line using pythagorean theorem
-    var length: CGFloat {
+    public var length: CGFloat {
         sqrt(
             pow(abs(point0.x - point1.x), 2) +
                 pow(abs(point0.y - point1.y), 2)
